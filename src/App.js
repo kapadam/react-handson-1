a19e48a286b388c2b4371c856d844385c2fefd27
Kapadam <dharanidharu211@gmail.com>
2023-01-13 20:46:27 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import User from "./User";
+import {useState} from 'react';
+import Header from "./Header";
+import Header1 from './Header1';
+import "./App.css";
 
 function App() {
+  // const handleClick=()=>{
+  //   setinc(show+1);
+  // }
+  // const handleReset=()=>{
+  //   setinc(0);
+  // }
+  // const[show,setinc]=useState();
+  const [showdiv,setShow]=useState(false)
+  const [showclass,setClass]=useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      <h1 className='heading'>Styling using Functional and Class Component</h1>
+      <div className='flexcontainer'>
+      <button onClick={()=>setClass(!showclass)}>To see styling in functional component</button>
+      <button onClick={()=>setShow(!showdiv)}>To see styling in class component</button>
+      </div>
+      <span>{showclass?<Header/>:" "}</span>
+        <span>{showdiv?<Header1/>:" "}</span>
+      </div>               
   );
 }
 
